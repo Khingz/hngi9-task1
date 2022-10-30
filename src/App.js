26abd62ec2bee753f91698cc694209cbb9129dc3
Khingz <kingsley995mr@gmail.com>
2022-10-30 10:29:18 +0100

    added title attribute to link

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import React, { useState } from 'react';
 import profile_img_default from './assets/img/profile_img_default.svg';
 // import profile_img_focus from './assets/img/profile_img_focus.svg';
-// import profile_img_hover from './assets/img/profile_img_hover.svg';
+import profile_img_hover from './assets/img/profile_img_hover.svg';
 import github_icon from './assets/img/Icon.svg';
 import slack_icon from './assets/img/slack.svg';
 import zuri_icon from './assets/img/Vector.svg';
 import hng_icon from './assets/img/Footer text.svg';
 import i4g_icon from './assets/img/I4G.svg';
 import share_icon from './assets/img/share_default.svg'
+import share_hover_icon from './assets/img/share_hover.svg';
+import share_mobile_icon from './assets/img/more_mobile.svg';
+import share_mobile_hover from './assets/img/more_mobile_focus.svg';
 
 import './App.css';
 
 function App() {
+  const [p_img, setP_img] = useState(true);
+  const [share, setShare] = useState(true);
+
   return (
     <div className="App">
       {/* profile image section */}
      <section id='profile__section'>
-      <img src={profile_img_default} alt="profile__img" id='twitter' onClick={() => console.log('clicked')}/>
+      <img src={p_img ? profile_img_default : profile_img_hover} alt="profile__img" id='twitter' onMouseEnter={() => setP_img(false)} onMouseLeave={() => setP_img(true)}/>
       <p id="twitter">team_DKs</p>
       <p id="slack">Khingz</p>
      </section>
@@ -23,11 +30,11 @@ function App() {
      {/* link section */}
      <section id='link__section'>
       <a href="https://twitter.com/team_Dks" className="link__btn">Twitter Link</a>
-      <a href="https://training.zuri.team/" id="btn__zuri" className="link__btn" >Books</a>
-      <a href="http://books.zuri.team" id="books" className="link__btn" title='Hello world'>Find Books</a>
-      <a href=" https://books.zuri.team/python-for-beginners?ref_id=Khingz" id="book__python" className="link__btn">Book Python</a>
-      <a href="https://background.zuri.team" id="pitch" className="link__btn">Pitch</a>
-      <a href=" https://books.zuri.team/design-rules" id="book__design" className="link__btn">Books</a>
+      <a href="https://training.zuri.team/" id="btn__zuri" className="link__btn" >Zuri Team</a>
+      <a href="http://books.zuri.team" id="books" className="link__btn" title='This is where you find books about design and coding'>Zuri Books</a>
+      <a href=" https://books.zuri.team/python-for-beginners?ref_id=Khingz" id="book__python" className="link__btn" title='Get best selling python books for beginners and professionals'>Python Books</a>
+      <a href="https://background.zuri.team" id="pitch" className="link__btn" title='Check proficiency and efficeincy of coders'>Background Check For Coders</a>
+      <a href=" https://books.zuri.team/design-rules" id="book__design" className="link__btn" title='Get free design books for beginers and professionals from zuri team'>Design Books</a>
      </section>
 
      {/* social section */}
@@ -44,7 +51,12 @@ function App() {
      </footer>
 
      {/* share button */}
-    <img src={share_icon} alt="share__btn" className='share__btn' />
+     <div className='share__container'>
+      <img src={share ? share_icon : share_hover_icon} alt="share__btn" className='share__btn share__desktop'  onMouseEnter={() => setShare(false)} onMouseLeave={() => setShare(true)}/>
+
+      <img src={share ? share_mobile_icon : share_mobile_hover} alt="share__btn" className='share__btn share__mobile'  onMouseEnter={() => setShare(false)} onMouseLeave={() => setShare(true)} />
+     </div>
+   
     </div>
   );
 }
