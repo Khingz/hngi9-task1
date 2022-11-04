7a556b9f3116b8fa2e610ab1bdfe34d3214cfc42
Khingz <kingsley995mr@gmail.com>
2022-11-04 13:00:50 +0100

    ui design completed

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,30 +1,56 @@
 import React, { useState } from 'react';
 
 const Contact = () => {
-    const [isValid, setIsValid] = useState(false)
+    const [isValid, setIsValid] = useState(false);
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [isError, setIsError] = useState(false);
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        validate();
+        if(isError == true) {
+
+        }
+    }
+
+    const validate = () => {
+        if(firstname.length <= 0 || lastname.length <=0 || email.length <= 0 || message.length <= 0) {
+            setIsError(true)
+        }
+    }
+
   return (
     <div className="contact__container">
       <div className="contact__content">
         <h3>Contact Me</h3>
         <p>Hi there, contact me to ask me about anything you have in mind</p>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="name__container">
                 <div className="fisttname__container">
                     <label htmlFor="firstname">First name</label>
-                    <input type="text" name="firstname" id="first_name" placeholder='Enter your first name' required/>
+                    <input type="text" name="firstname" id="first_name" placeholder='Enter your first name' value={firstname} onChange={(e) => setFirstname(e.target.value)}/>
+                    {isError && firstname <= 0 ? <p className='error'>Please enter your first name</p> : ''}   
                 </div>
                 <div className="lastname__container">
                     <label htmlFor="lastname">Last name</label>
-                    <input type="text" name="lastname" id="last_name" placeholder='Enter your last name' required/>
+                    <input type="text" name="lastname" id="last_name" placeholder='Enter your last name' value={lastname} onChange={(e) => setLastname(e.target.value)}/>
+                    {isError && firstname <= 0 ? <p className='error'>Please enter your last name</p> : ''}   
                 </div>
             </div>
             <div className="email__container">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder='yourname@email.com' required/>
+                    <input type="email" name="email" id="email" placeholder='yourname@email.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    {isError && firstname <= 0 ? <p className='error'>Please enter your email</p> : ''}
+                    
             </div>
             <div className="message__container">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" placeholder='Send a message and i will reply you as soon as possible'></textarea>
+            <textarea name="message" id="message" placeholder='Send a message and i will reply you as soon as possible'value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+            {isError && firstname <= 0 ? <p className='error'>Please enter a message</p> : ''}
             </div>
             <div className="agree__container">
                 <input type="checkbox" name="check" id="agree" onClick={() => setIsValid(!isValid)}/>
