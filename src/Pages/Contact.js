1567201663b9ec1852155a6a2980a814d2998ddf
Khingz <kingsley995mr@gmail.com>
2022-11-04 11:05:58 +0100

    feat: disable submit button when checkbox is not checked - contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
+    const [isValid, setIsValid] = useState(false)
   return (
     <div className="contact__container">
       <div className="contact__content">
@@ -10,27 +11,27 @@ const Contact = () => {
             <div className="name__container">
                 <div className="fisttname__container">
                     <label htmlFor="firstname">First name</label>
-                    <input type="text" name="firstname" id="first_name" placeholder='Enter your first name'/>
+                    <input type="text" name="firstname" id="first_name" placeholder='Enter your first name' required/>
                 </div>
                 <div className="lastname__container">
                     <label htmlFor="lastname">Last name</label>
-                    <input type="text" name="lastname" id="last_name" placeholder='Enter your last name'/>
+                    <input type="text" name="lastname" id="last_name" placeholder='Enter your last name' required/>
                 </div>
             </div>
             <div className="email__container">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder='yourname@email.com'/>
+                    <input type="email" name="email" id="email" placeholder='yourname@email.com' required/>
             </div>
             <div className="message__container">
             <label htmlFor="message">Message</label>
             <textarea name="message" id="message" placeholder='Send a message and i will reply you as soon as possible'></textarea>
             </div>
             <div className="agree__container">
-                <input type="checkbox" name="check" id="agree" />
+                <input type="checkbox" name="check" id="agree" onClick={() => setIsValid(!isValid)}/>
                 <span>You agree to provide your data to Khingz who may contact you</span>
             </div>
             <div className="submit__btn__container">
-                <button type="submit">Send message</button>
+                <button type="submit" disabled={!isValid}>Send message</button>
             </div>
         </form>
       </div>
