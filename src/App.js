343d152d9cc88f093d41fa34e7ccda13e444fdfb
Khingz <kingsley995mr@gmail.com>
2022-11-04 15:14:13 +0100

    fix: added a scroll to top component for page scrolling

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import ProfileHome from './Pages/Profile_Home';
 import Footer from './component/footer';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import ScrollToTop from './component/scrolTop';
 import Contact from './Pages/Contact';
 
 function App() {
   return (
      <div className="App">
       <Router>
-        <Routes>
-          <Route path='/' element={<ProfileHome />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+              <Route path='/' element={<ProfileHome />} />
+              <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </ScrollToTop>
       </Router>
       <Footer /> 
      </div>
